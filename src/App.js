26d16fec8c85e524b5eff0cb656e3d1965ae9bc7
1taegon <wlwhsxorhs@naver.com>
2023-02-20 10:39:36 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/abot-us" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
